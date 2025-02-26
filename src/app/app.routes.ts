883191abe7d227aacff1a134f9eeae83772a6cda
Bhargav Mule <bhargavmule16@gmail.com>
2025-02-26 23:15:32 +0530

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { PricingComponent } from './pricing/pricing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingpageComponent , title: 'Landingpage'},
+    {path:'pricing', component: PricingComponent, title: 'Pricing'}
+];
